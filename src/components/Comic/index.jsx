0eaca6c44e973e72f43ab3ffe3ad501ec8926b87
james-harper <jameswilliamharper@gmail.com>
2020-07-06 12:37:12 +0100

    Link to xkcd.com page from each image

--- src/components/Comic/index.jsx
+++ src/components/Comic/index.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 const Comic = ({ xkcd }) => (
   <div>
     <h2>{xkcd.title}</h2>
-    <img
-      src={xkcd.img}
-      alt={xkcd.alt}
-      title={xkcd.alt}
-    />
+    <a
+      href={`https://xkcd.com/${xkcd.num}/`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={xkcd.img}
+        alt={xkcd.alt}
+        title={xkcd.alt}
+      />
+    </a>
   </div>
 );
 
